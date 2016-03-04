c30538c627ce32e36e17ad06ed93a2db04111853
Nathan Baldwin <nathansbaldwin@gmail.com>
2016-03-04 09:03:53 -0600

    front-end file structure complete with grunt and sass

--- public/our-list-app/Gruntfile.js
+++ public/our-list-app/Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-          '../styles/main.css': '../sass/main.scss'
+          './styles/main.css': './sass/main.scss'
         }
       }
     },
